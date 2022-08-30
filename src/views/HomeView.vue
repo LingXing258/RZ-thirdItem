0380424a5c6b0977490cfafe1a1ad669ee2e8401
Lingxing258 <1617118587@qq.com>
2022-08-30 09:39:49 +0800

    初始化

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="device-container">
+    <div class="paading-container">
+      <div class="card-container">
+     </div>
+    </div>
   </div>
 </template>
-
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  data () {
+
   }
 }
 </script>
+<style lang="scss" scoped>
+  .device-container {
+    width: 100%;
+    height: 50px;
+    background-color: blue;
+  }
+</style>
